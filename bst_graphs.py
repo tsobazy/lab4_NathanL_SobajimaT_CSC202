f6efbcd62679407903599ac87153de51616c3940
Nathan Lee <NathanLee4272@gmail.com>
2025-11-15 21:51:19 -0800

    finished

--- bst_graphs.py
+++ bst_graphs.py
@@ -6,6 +6,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import time
 sys.setrecursionlimit(10**6)
 
 from bst import *
@@ -73,6 +74,7 @@ def height(tree : BinTree) -> int:
 
 # generate graph of average tree height as a function of N where n_max is 49
 def random_tree_graph() -> None:
+    TREES_PER_RUN : int = 10000
     y_vals : list[float] = []
     for i in range(1, 50):
         tot_h = 0
@@ -96,11 +98,47 @@ def random_tree_graph() -> None:
     plt.legend() # makes the 'label's show up
     plt.show()
 
+# generate graph of inserting random value into random tree of size N as a 
+# function of N where n_max is 199
+def insert_random_tree_graph() -> None:
+    TREES_PER_RUN : int = 10000
+    y_vals : list[float] = []
+    for i in range(1, 50):
+        tot_time : float = 0
+        for _ in range(TREES_PER_RUN):
+            tree = random_tree(i - 1)
+            start = time.perf_counter()
+            tree = insert(tree, random.random())
+            end = time.perf_counter()
+
+            tot_time += (end - start)
+        avg_time = tot_time / TREES_PER_RUN
+        y_vals.append(avg_time)
+    
+    x_coords : list[int] = [int(i - 1) for i in range(1, 50)]
+    y_coords : list[float] = y_vals
+
+    x_numpy : np.ndarray = np.array(x_coords)
+    y_numpy : np.ndarray = np.array(y_coords)
+        
+    plt.plot( x_numpy, y_numpy, label = 'n_max = 49')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Time of Inserting Random Value Into Average Tree of Size N as a Function of N')
+    plt.grid(True)
+    plt.legend() # makes the 'label's show up
+    plt.show()
+
 class BSTTests(unittest.TestCase):
-    def test_n_max(self):
+    # def test_n_max(self):
+    #     for _ in range(TREES_PER_RUN):
+    #         tree = random_tree(49)
+    #         h = height(tree.BT)
+
+    def test_insert_rand(self):
         for _ in range(TREES_PER_RUN):
             tree = random_tree(49)
-            h = height(tree.BT)
+            tree = insert(tree, random.random())
 
 if (__name__ == '__main__'):
-    random_tree_graph()
+    insert_random_tree_graph()
