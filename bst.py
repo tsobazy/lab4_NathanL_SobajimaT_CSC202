abd7db367dbc0777cff09a63ef5434e999502fbd
Nathan Lee <NathanLee4272@gmail.com>
2025-11-15 19:18:09 -0800

    finished 2.1

--- bst.py
+++ bst.py
@@ -4,32 +4,35 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6) 
 
-#2.1
-BinTree: TypeAlias = Optional["BTNode"]
+# 2.1
+BinTree : TypeAlias = Union['BTNode', None]
 
 @dataclass(frozen=True)
 class BTNode:
-    value: Any
-    left: BinTree
-    right: BinTree
+    value : Any
+    left : BinTree
+    right : BinTree
+
+def comes_before_function(a : Any, b : Any) -> bool:
+    return a < b
 
 @dataclass(frozen=True)
 class BST:
-    comes_before: Callable[[Any, Any], bool]
-    BT: BinTree 
+    comes_before : Callable[[Any, Any], bool]
+    BT : BinTree 
 
 
-# returns True if the BST is empty, False otherwise.
-def is_empty(t: BST) -> bool:
+# finds whether a binary search tree is empty (True means empty)
+def is_empty(t : BST) -> bool:
     if t.BT is None:
         return True
     return False
 
-# adds a given value to the binary search tree
-def insert(t: BST, val: Any) -> BST:
-    decide = t.comes_before  # user-supplied comparator stored on the tree
+# adds a given value to a binary search tree
+def insert(t : BST, val : Any) -> BST:
+    decide = t.comes_before
                  
-    def insert_helper(t: BinTree, val: Any, decide: Callable[[Any, Any], bool] ) ->  BinTree:
+    def insert_helper(t : BinTree, val : Any, decide : Callable[[Any, Any], bool]) ->  BinTree:
         if t is None:
             return BTNode(val, None, None)
         if decide(val, t.value):
@@ -37,9 +40,50 @@ def insert(t: BST, val: Any) -> BST:
         if decide(t.value, val):
             return BTNode(t.value, t.left, insert_helper(t.right, val, decide))
         return t
+    
     return BST(decide, insert_helper(t.BT, val, decide))
 
-# returns True if it is stored in the tree and False otherwise
-def lookup
-    
+# finds whether a value is in a binary search tree (True means value found in tree)
+def lookup(t : BST, val : Any) -> bool:
+    decide = t.comes_before
 
+    def lookup_helper(t : BinTree, val : Any, decide : Callable[[Any, Any], bool]) -> bool:
+        if t is None:
+            return False
+        
+        if not decide(val, t.value) and not decide(t.value, val):
+            return True
+        if decide(val, t.value):
+            return lookup_helper(t.left, val, decide)
+        if decide(t.value, val):
+            return lookup_helper(t.right, val, decide)
+        return False
+    
+    return lookup_helper(t.BT, val, decide)
+
+# deletes, if possible, a value from a binary search tree and generates new tree
+def delete(t : BST, val : Any) -> BST:
+    decide = t.comes_before
+
+    def delete_helper(t : BinTree, val : Any) -> BinTree:
+        if t is None:
+            return None
+        
+        if not decide(val, t.value) and not decide(t.value, val):
+            if t.left is None:
+                return t.right
+            if t.right is None:
+                return t.left
+        
+            success = t.right
+            while success.left is not None:
+                success = success.left
+            new_right = delete_helper(t.right, success.value)
+            return BTNode(success.value, t.left, new_right)
+
+        if decide(val, t.value):
+            return BTNode(t.value, delete_helper(t.left, val), t.right)
+        else:
+            return BTNode(t.value, t.left, delete_helper(t.right, val))
+        
+    return BST(decide, delete_helper(t.BT, val))
--- bst_tests.py
+++ bst_tests.py
@@ -3,9 +3,25 @@ import unittest
 from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
+
 from bst import *
+
+num_bst = BST(comes_before_function, BTNode(5, BTNode(3, 
+                                                      BTNode(2, None, None), 
+                                                      BTNode(4, None, None)),
+                                                BTNode(11, None, BTNode(15, None, None))))
+
+alph_bst = BST(comes_before_function, BTNode('h', BTNode('b', 
+                                                      BTNode('a', None, None), 
+                                                      BTNode('f', None, None)),
+                                                BTNode('x', 
+                                                       BTNode('n', None, None), 
+                                                       BTNode('z', None, None))))
+
 class BSTTests(unittest.TestCase):
- def test_example(self):
- pass
+   def test_is_empty(self):
+     self.assertEqual(False, is_empty(num_bst))
+     self.assertEqual(True, is_empty(BST(comes_before_function, None)))
+   
 if (__name__ == '__main__'):
  unittest.main() 
